b5f07b65bfd3f4e8285651f99ec095eea7327f33
tavisca-sjana <sjana@tavisca.in>
2019-06-20 11:52:11 -0700

    Added code

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
@@ -41,7 +42,132 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            //To keep track of the meals where the initial nutrients are same
+            List<int> same = new List<int>();
+            int foodItemsNum = protein.Length;
+            //To keep track of the characters remaining to parse in each dietPlan[i]
+            int letterLength;
+            
+            int [] cal = new int[foodItemsNum];
+            int [] mealPlan = new int[dietPlans.Length];
+            
+            //Calculating Calories
+            for(int i=0;i<foodItemsNum;i++)
+            {
+                cal[i] = 5*protein[i] + 5*carbs[i] + 9*fat[i];
+            }
+
+            for(int i=0;i<dietPlans.Length;i++)
+            {
+                same.Clear();
+                for(int j=0;j<foodItemsNum;j++)
+                    same.Add(j);
+                letterLength = dietPlans[i].Length;
+                foreach(var letter in dietPlans[i])
+                {
+                    
+                    switch(letter)
+                    {
+                        case 'c':
+                            mealPlan[i] = GetIndex(letter,carbs,ref same,letterLength);
+                            break;
+                        case 'C':
+                            mealPlan[i] = GetIndex(letter,carbs,ref same,letterLength);
+                            break;
+                        case 'p':
+                            mealPlan[i] = GetIndex(letter,protein,ref same,letterLength);
+                            break;
+                        case 'P':
+                            mealPlan[i] = GetIndex(letter,protein,ref same,letterLength);
+                            break;
+                        case 't':
+                            mealPlan[i] = GetIndex(letter,cal,ref same,letterLength);
+                            break;
+                        case 'T':
+                            mealPlan[i] = GetIndex(letter,cal,ref same,letterLength);
+                            break;
+                        case 'f':
+                            mealPlan[i] = GetIndex(letter,fat,ref same,letterLength);
+                            break;
+                        case 'F':
+                            mealPlan[i] = GetIndex(letter,fat,ref same,letterLength);
+                            break;
+                    }
+                   
+                    if(mealPlan[i] != -1)
+                        break;
+                    letterLength -= 1;
+                }
+            }
+
+            return mealPlan;
+
+
         }
+
+        private static int GetIndex(char letter,int[] nutrient,ref List<int> same,int letterLength)
+        {
+            List<int> temp = new List<int>();
+            int index=0,max,min;
+            
+            //Max nutrient value for uppercase nutrients
+            if(char.IsUpper(letter))
+            {
+    
+                max = int.MinValue;
+                foreach(var i in same)
+                {
+                    if(nutrient[i]>max)
+                    {
+                        max = nutrient[i];
+                        index = i;
+                    }
+            
+                }
+                //same.Clear();
+
+                 foreach(var j in same)
+                {
+                    if(nutrient[j] == max)
+                    {
+                        temp.Add(j);
+
+                    }
+                }
+                same = temp;
+
+            }
+            //Min nutrient value for lowercase nutrients
+            else
+            {
+                min = int.MaxValue;
+                foreach(var i in same)
+                {
+                    if(nutrient[i]<min)
+                    {
+                        min = nutrient[i];
+                        index = i;
+                    }
+                    //same.Clear();
+                }
+
+                     foreach(var j in same)
+                    {
+                        if(nutrient[j] == min)
+                            temp.Add(j);
+                    }
+
+                   same = temp;     
+            }
+
+        
+        
+
+        
+            if(same.Count > 1 && letterLength != 1)
+                return -1;
+            return index;
     }
+  }
+
 }
